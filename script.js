6163ed0e1192f81c7a6cacb75dabb8fdbd1db3bd
bnasare <124492150+bnasare@users.noreply.github.com>
2023-12-07 02:00:21 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.addEventListener('scroll', function () {
+//     var header = document.querySelector('.header');
+//     var quiz = document.querySelector('.quiz');
+//     var headerHeight = header.offsetHeight;
+
+//     quiz.style.marginTop = headerHeight + 'px';
+// });
+
+
