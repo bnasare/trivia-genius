5a501b48ba83192e52c9d85f5e63323b9cf5719b
bnasare <124492150+bnasare@users.noreply.github.com>
2023-12-07 03:32:21 +0000

    update

--- script.js
+++ script.js
@@ -6,4 +6,32 @@
 //     quiz.style.marginTop = headerHeight + 'px';
 // });
 
+const loginButton = document.getElementById('loginButton');
+
+function redirectToQuiz() {
+    window.history.replaceState({}, '', '/quiz.html');
+    window.location.href = '/quiz.html';
+}
+
+loginButton.addEventListener('click', function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get username and password from input fields
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (username === 'admin' && password === 'admin') {
+        alert('Login successful!');
+        console.log('Attempting to redirect...');
+        redirectToQuiz();
+    } else {
+        alert('Login failed!');
+    }
+
+    usernameInput.value = '';
+    passwordInput.value = '';
+});
 
